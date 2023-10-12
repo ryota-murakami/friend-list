d44e98a988297053fbcaffae691f1061149cecbc
ryota-murakami <dojce1048@gmail.com>
2023-10-12 09:54:34 +0900

    </NavLink>

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import {
   Form,
   Link,
   Links,
+  NavLink,
   LiveReload,
   Outlet,
   Meta,
@@ -60,16 +61,23 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{' '}
-                      {contact.favorite ? <span>★</span> : null}
-                    </Link>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? 'active' : isPending ? 'pending' : ''
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{' '}
+                        {contact.favorite ? <span>★</span> : null}
+                      </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
