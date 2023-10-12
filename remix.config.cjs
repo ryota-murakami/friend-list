e7acf402e41a887489f71fb268eab7ba8f6106b0
ryota-murakami <dojce1048@gmail.com>
2023-10-12 21:28:28 +0900

    v2

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,9 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  future: {
-    v2_dev: true,
-    v2_normalizeFormMethod: true,
-  },
   ignoredRouteFiles: ['**/.*'],
   serverModuleFormat: 'cjs',
 }
