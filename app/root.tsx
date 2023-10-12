bddec8caea8990749190ca675771fe8a363e7399
ryota-murakami <dojce1048@gmail.com>
2023-10-12 12:18:52 +0900

    global pendingui

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from '@remix-run/react'
 
 import appStylesHref from './app.css'
@@ -30,6 +31,8 @@ export const loader = async () => {
 }
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
+
   return (
     <html lang="en">
       <head>
@@ -88,7 +91,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === 'loading' ? 'loading' : ''}
+          id="detail"
+        >
           <Outlet />
         </div>
 
